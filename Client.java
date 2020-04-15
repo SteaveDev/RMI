9a0634ec9b18cc337b88df79f6a1e1fa6a775860
Steave <56349889+SteaveDev@users.noreply.github.com>
2020-04-15 13:02:43 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,19 +5,12 @@ public class Client{
 	public static void main(String [] args) throws Exception{
 		
         Data d = (Data) Naming.lookup("rmi://localhost/33333");
-        System.out.println("Client connecté.");
-        
-        d.start();
+        System.out.println("Client connectÃ©.");
         
         while (true) {
-            d.majScoreTempsReel();
             try {
-            	
                 Thread.sleep(1000);
-                d.majScoreMessage();
                 System.out.println(d.meilleurs());
-                //System.out.println(d.afficheMessage());
-                //System.out.println(d.afficheCommentaire());
             }
             catch (InterruptedException e) {
                 e.printStackTrace();
